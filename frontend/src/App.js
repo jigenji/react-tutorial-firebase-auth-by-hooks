9369acb52d83f51ca8890111d83443eccd3a04ff
Shinji Kawakami <skphysi929@gmail.com>
2021-01-06 14:40:30 +0900

    add the basic react app using bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CustomNavbar from './components/CustomNavbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CustomNavbar />
+    </div>
+  );
+}
+
+export default App;
